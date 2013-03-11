e05bd73ee3991e426d778b3b1bcdaf1d6f203365
duan.houhai <duan.houhai@denachina.com>
2013-03-11 12:11:06 +0800

    less token

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports={
 	base:{
 		Core:require("./base/Core").Core,
-		ArgParser:require("./base/ArgParser").ArgParser
+		ArgParser:require("./base/ArgParser").ArgParser,
 		BaseObject:require("./base/BaseObject").BaseObject
 	}
 };
