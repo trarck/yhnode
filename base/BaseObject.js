1805544a569056af07eee195d6d2dfdff7db2896
duan.houhai <duan.houhai@denachina.com>
2013-03-11 12:06:41 +0800

    add BaseObject

---
+++ base/BaseObject.js
@@ -0,0 +1,6 @@
+var BaseObject=exports.BaseObject=function(){
+	this.initialize.apply(this,arguments);
+};
+BaseObject.prototype.initialize=function(){
+	
+};
--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports={
 	base:{
 		Core:require("./base/Core").Core,
 		ArgParser:require("./base/ArgParser").ArgParser
+		BaseObject:require("./base/BaseObject").BaseObject
 	}
 };
